06d3e26ac9058d9186fc60d5d96e5e7e95e94f28
arthurgranito <arthurcursotecnico@gmail.com>
2025-05-20 11:27:24 -0300

    Order,Instant, ISO 8601 - Pt. 1

--- src/main/java/com/arthurgranito/project_springboot_jpa_hibernate/entities/User.java
+++ src/main/java/com/arthurgranito/project_springboot_jpa_hibernate/entities/User.java
@@ -3,6 +3,8 @@ package com.arthurgranito.project_springboot_jpa_hibernate.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,7 +19,10 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
-    public User(){
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User() {
     }
 
     public User(Long id, String name, String email, String phone, String password) {
@@ -66,6 +71,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
