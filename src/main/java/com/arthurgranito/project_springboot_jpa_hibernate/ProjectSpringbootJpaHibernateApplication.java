f26ec614c1c28281ccd8a19b85db550231122538
arthurgranito <arthurcursotecnico@gmail.com>
2025-05-20 09:03:20 -0300

    Project Created

---
+++ src/main/java/com/arthurgranito/project_springboot_jpa_hibernate/ProjectSpringbootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.arthurgranito.project_springboot_jpa_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootJpaHibernateApplication.class, args);
+	}
+
+}
