15f38199491ce6ac69d39c25273cade0b2fd179b
arthurgranito <arthurcursotecnico@gmail.com>
2025-05-20 23:32:42 -0300

    User insert

--- src/main/java/com/arthurgranito/project_springboot_jpa_hibernate/resources/UserResource.java
+++ src/main/java/com/arthurgranito/project_springboot_jpa_hibernate/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.arthurgranito.project_springboot_jpa_hibernate.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 
@@ -28,5 +30,13 @@ public class UserResource {
         User user = service.findById(id);
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = service.insert(user);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/${id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
